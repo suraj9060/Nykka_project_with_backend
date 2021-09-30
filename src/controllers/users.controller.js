b2b12f7b7a52441e1f7108fc33026fe312adccfe
Sanket Chaware <sanketnchaware@gmail.com>
2021-09-30 14:50:27 +0530

    signup page

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,14 +1,16 @@
 const express = require('express');
 
 const User = require('../models/users.model')
+const crudController = require('./crud.controller')
 
 const router = express.Router()
 
+router.post("/signup/post", crudController.post(User))
 
 router.get("/login", async (req, res) => {
      const user = await User.find().lean().exec();
 
-    return res.render('login1', {
+    return res.render('signup', {
         user:user
     })
 });
