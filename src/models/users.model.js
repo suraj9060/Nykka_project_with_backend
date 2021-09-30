745316410f31403c365e2e27f1758bae4a0adc9e
RaniKamna <kamna.a139@gmail.com>
2021-09-30 11:40:56 +0530

    user model 1

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -1,14 +1,17 @@
 const mongoose = require('mongoose');
 
+const Product = require("./products.model");
+
 const usersSchema = new mongoose.Schema({
     first_name: { type: String, required: true },
-    last_name: {type: String, required: true},
+    last_name: { type: String, required: true },
     phone_number: { type: Number, required: true },
-    email: {type: String, required: true},
+    email: { type: String, required: true },
     gender: { type: String, required: true },
-    product_id: { type: Number, required: true },
+    product_id: { type: Number, required: false },
+    password: { type: String, required: true },
+    address: { type: String, required: true },
 });
-
 
 const User = mongoose.model("user", usersSchema);
 
